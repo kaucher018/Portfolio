9e5006be2c9f91cfe83988f297c24b4b9ef37d3a
The Kaucher <muhammadkaucher018@gmail.com>
2025-06-19 21:27:20 +0600

    project setup

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import NavbarMain from "./components/navbar/NavbarMain";
+
+
+function App() {
+
+
+  return (
+    <>
+     <NavbarMain/>
+    </>
+  )
+}
+
+export default App
