ad4a9a903ef4371048160d71bf742a896fe632a9
The Kaucher <muhammadkaucher018@gmail.com>
2025-06-27 21:15:12 +0600

    some updates, added photos

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,6 +2,14 @@
 import './App.css'
 import NavbarMain from "./components/navbar/NavbarMain";
 import HeroMain from "./components/heroSection/HeroMain";
+import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
+import FooterMain from "./components/footer/FooterMain";
+import HeroGradient from "./components/heroSection/HeroGradient";
+import ProjectsMain from "./components/projectsSection/ProjectsMain";
+import SkillsMain from "./components/skillsSection/SkillsMain";
+import SubSkills from "./components/skillsSection/SubSkills";
+import SubHeroMain from "./components/subHeroSection/SubHeroMain";
+import ContactMeMain from "./Components/contactMeSection/ContactMeMain";
 
 
 function App() {
@@ -11,6 +19,14 @@ function App() {
     <>
      <NavbarMain/>
      <HeroMain/>
+      <HeroGradient />
+      <SubHeroMain />
+      <AboutMeMain />
+      <SkillsMain />
+      <SubSkills />
+      <ProjectsMain />
+      <ContactMeMain />
+      <FooterMain />
     </>
   )
 }
